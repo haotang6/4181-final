e9406b7ec2a103825ca97f0a0587093991d87c3d
Weifan Jiang <weifanjiang@hotmail.com>
2020-12-17 14:58:49 -0500

    add test server code

---
+++ server-credentials.sh
@@ -0,0 +1,3 @@
+openssl ecparam -genkey -name prime256v1 -noout -out server-private-key.pem
+openssl ec -in server-private-key.pem -pubout -out server-public-key.pem
+openssl req -new -x509 -sha256 -key server-private-key.pem -subj "/CN=duckduckgo.com" -out server-certificate.pem
