4fd2813814bbddb65251eaa656cc6683fb11d96d
Weifan Jiang <weifanjiang@hotmail.com>
2020-12-19 04:34:31 -0500

    bug fix

--- getcert.cpp
+++ getcert.cpp
@@ -7,6 +7,18 @@
 #include <openssl/x509v3.h>
 
 int main(int argc, char *argv[]) {
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+    SSL_library_init();
+    SSL_load_error_strings();
+#endif
+
+    /* Set up the SSL context */
+
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+    auto ctx = my::UniquePtr<SSL_CTX>(SSL_CTX_new(SSLv23_client_method()));
+#else
+    auto ctx = my::UniquePtr<SSL_CTX>(SSL_CTX_new(TLS_client_method()));
+#endif
 
     // format request to server
     my::send_http_request(ssl_bio.get(), "GET / HTTP/1.1", "duckduckgo.com");
