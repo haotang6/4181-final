fab13f4518316aedef6db50ccaf308464f65699f
Weifan Jiang <weifanjiang@hotmail.com>
2020-12-19 04:31:46 -0500

    bug fix

--- getcert.cpp
+++ getcert.cpp
@@ -1,6 +1,10 @@
 // Usage: ./getcert username password public_key_file certificate_file
 
 #include "client_helper.hpp"
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+#include <openssl/x509v3.h>
 
 int main(int argc, char *argv[]) {
 
