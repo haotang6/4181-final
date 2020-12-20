ee80168fff48b29e0cd1863c6db914154cd73e2f
Weifan Jiang <weifanjiang@hotmail.com>
2020-12-19 20:39:12 -0500

    move CA

--- step1.sh
+++ step1.sh
@@ -1,51 +1,44 @@
 #!/bin/bash
+rm -rf ~/ca
 
-export root="./ca"
-
-rm -rf $root
+# current directory
+export cwd=$(pwd)
 
 # root CA
-mkdir $root
-cd $root
-# root CA config file 
+mkdir ~/ca
+cd ~/ca
+# root CA config file
 echo -n '# OpenSSL root CA configuration file.
-# Copy to `$root/openssl.cnf`.
-
+# Copy to `~/ca/openssl.cnf`.
 [ ca ]
 # `man ca`
 default_ca = CA_default
-
 [ CA_default ]
 # Directory and file locations.
 dir               = ' >> openssl.cnf
-echo $root >> openssl.cnf
-echo '
+echo -n $HOME >> openssl.cnf
+echo '/ca
 certs             = $dir/certs
 crl_dir           = $dir/crl
 new_certs_dir     = $dir/newcerts
 database          = $dir/index.txt
 serial            = $dir/serial
 RANDFILE          = $dir/private/.rand
-
 # The root key and root certificate.
 private_key       = $dir/private/ca.key.pem
 certificate       = $dir/certs/ca.cert.pem
-
 # For certificate revocation lists.
 crlnumber         = $dir/crlnumber
 crl               = $dir/crl/ca.crl.pem
 crl_extensions    = crl_ext
 default_crl_days  = 30
-
 # SHA-1 is deprecated, so use SHA-2 instead.
 default_md        = sha256
-
 name_opt          = ca_default
 cert_opt          = ca_default
 default_days      = 375
 preserve          = no
 policy            = policy_strict
-
 [ policy_strict ]
 # The root CA should only sign intermediate certificates that match.
 # See the POLICY FORMAT section of `man ca`.
@@ -55,7 +48,6 @@ organizationName        = match
 organizationalUnitName  = optional
 commonName              = supplied
 emailAddress            = optional
-
 [ policy_loose ]
 # Allow the intermediate CA to sign a more diverse range of certificates.
 # See the POLICY FORMAT section of the `ca` man page.
@@ -66,20 +58,16 @@ organizationName        = optional
 organizationalUnitName  = optional
 commonName              = supplied
 emailAddress            = optional
-
 [ req ]
 # Options for the `req` tool (`man req`).
 prompt              = no
 default_bits        = 2048
 distinguished_name  = req_distinguished_name
 string_mask         = utf8only
-
 # SHA-1 is deprecated, so use SHA-2 instead.
 default_md          = sha256
-
 # Extension to add when the -x509 option is used.
 x509_extensions     = v3_ca
-
 [ req_distinguished_name ]
 # See <https://en.wikipedia.org/wiki/Certificate_signing_request>.
 countryName                     = US
@@ -89,21 +77,18 @@ localityName                    = Manhattan
 organizationalUnitName          = Haotang Ltd Certificate Authority
 commonName                      = Haotang Ltd Root CA
 emailAddress                    = hl3311@columbia.edu
-
 [ v3_ca ]
 # Extensions for a typical CA (`man x509v3_config`).
 subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid:always,issuer
 basicConstraints = critical, CA:true
 keyUsage = critical, digitalSignature, cRLSign, keyCertSign
-
 [ v3_intermediate_ca ]
 # Extensions for a typical intermediate CA (`man x509v3_config`).
 subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid:always,issuer
 basicConstraints = critical, CA:true, pathlen:0
 keyUsage = critical, digitalSignature, cRLSign, keyCertSign
-
 [ usr_cert ]
 # Extensions for client certificates (`man x509v3_config`).
 basicConstraints = CA:FALSE
@@ -113,7 +98,6 @@ subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid,issuer
 keyUsage = critical, nonRepudiation, digitalSignature, keyEncipherment
 extendedKeyUsage = clientAuth, emailProtection
-
 [ server_cert ]
 # Extensions for server certificates (`man x509v3_config`).
 basicConstraints = CA:FALSE
@@ -123,11 +107,9 @@ subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid,issuer:always
 keyUsage = critical, digitalSignature, keyEncipherment
 extendedKeyUsage = serverAuth
-
 [ crl_ext ]
 # Extension for CRLs (`man x509v3_config`).
 authorityKeyIdentifier=keyid:always
-
 [ ocsp ]
 # Extension for OCSP signing certificates (`man ocsp`).
 basicConstraints = CA:FALSE
@@ -148,54 +130,47 @@ openssl req -config openssl.cnf \
       -passin pass:1234 \
       -new -x509 -days 7300 -sha256 -extensions v3_ca \
       -out certs/ca.cert.pem
-      
+
 chmod 444 certs/ca.cert.pem
 openssl x509 -noout -text -in certs/ca.cert.pem
 
 # intermediate CA
-mkdir intermediate
-cd intermediate
+mkdir ~/ca/intermediate
+cd ~/ca/intermediate
 # intermediate CA conf
 echo -n '# OpenSSL intermediate CA configuration file.
-# Copy to `$root/intermediate/openssl.cnf`.
-
+# Copy to `~/ca/intermediate/openssl.cnf`.
 [ ca ]
 # `man ca`
 default_ca = CA_default
-
 [ CA_default ]
 # Directory and file locations.
 dir               = ' >> openssl.cnf
-echo -n $root >> openssl.cnf
+echo -n $HOME >> openssl.cnf
 
 
-echo '/intermediate
+echo '/ca/intermediate
 certs             = $dir/certs
 crl_dir           = $dir/crl
 new_certs_dir     = $dir/newcerts
 database          = $dir/index.txt
 serial            = $dir/serial
 RANDFILE          = $dir/private/.rand
-
 # The root key and root certificate.
 private_key       = $dir/private/intermediate.key.pem
 certificate       = $dir/certs/intermediate.cert.pem
-
 # For certificate revocation lists.
 crlnumber         = $dir/crlnumber
 crl               = $dir/crl/intermediate.crl.pem
 crl_extensions    = crl_ext
 default_crl_days  = 30
-
 # SHA-1 is deprecated, so use SHA-2 instead.
 default_md        = sha256
-
 name_opt          = ca_default
 cert_opt          = ca_default
 default_days      = 375
 preserve          = no
 policy            = policy_loose
-
 [ policy_strict ]
 # The root CA should only sign intermediate certificates that match.
 # See the POLICY FORMAT section of `man ca`.
@@ -205,7 +180,6 @@ organizationName        = match
 organizationalUnitName  = optional
 commonName              = supplied
 emailAddress            = optional
-
 [ policy_loose ]
 # Allow the intermediate CA to sign a more diverse range of certificates.
 # See the POLICY FORMAT section of the `ca` man page.
@@ -216,20 +190,16 @@ organizationName        = optional
 organizationalUnitName  = optional
 commonName              = supplied
 emailAddress            = optional
-
 [ req ]
 # Options for the `req` tool (`man req`).
 prompt              = no
 default_bits        = 2048
 distinguished_name  = req_distinguished_name
 string_mask         = utf8only
-
 # SHA-1 is deprecated, so use SHA-2 instead.
 default_md          = sha256
-
 # Extension to add when the -x509 option is used.
 x509_extensions     = v3_ca
-
 [ req_distinguished_name ]
 # See <https://en.wikipedia.org/wiki/Certificate_signing_request>.
 countryName                     = US
@@ -239,21 +209,18 @@ localityName                    = Manhattan
 organizationalUnitName          = Haotang Ltd Certificate Authority
 commonName                      = Haotang Ltd Intermediate CA
 emailAddress                    = hl3311@columbia.edu
-
 [ v3_ca ]
 # Extensions for a typical CA (`man x509v3_config`).
 subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid:always,issuer
 basicConstraints = critical, CA:true
 keyUsage = critical, digitalSignature, cRLSign, keyCertSign
-
 [ v3_intermediate_ca ]
 # Extensions for a typical intermediate CA (`man x509v3_config`).
 subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid:always,issuer
 basicConstraints = critical, CA:true, pathlen:0
 keyUsage = critical, digitalSignature, cRLSign, keyCertSign
-
 [ usr_cert ]
 # Extensions for client certificates (`man x509v3_config`).
 basicConstraints = CA:FALSE
@@ -263,7 +230,6 @@ subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid,issuer
 keyUsage = critical, nonRepudiation, digitalSignature, keyEncipherment
 extendedKeyUsage = clientAuth, emailProtection
-
 [ server_cert ]
 # Extensions for server certificates (`man x509v3_config`).
 basicConstraints = CA:FALSE
@@ -273,7 +239,6 @@ subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid,issuer:always
 keyUsage = critical, digitalSignature, keyEncipherment
 extendedKeyUsage = serverAuth
-
 [ encrypt_file ]
 basicConstraints = CA:FALSE
 nsCertType = client, email
@@ -281,7 +246,6 @@ nsComment = "OpenSSL Generated Certificate for Encrypting Files to Me"
 subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid,issuer
 keyUsage = critical, digitalSignature, keyEncipherment, dataEncipherment, keyAgreement, encipherOnly
-
 [ sign_file ]
 basicConstraints = CA:FALSE
 nsCertType = client, email, objsign
@@ -289,12 +253,9 @@ nsComment = "OpenSSL Generated Certificate for Signing Files"
 subjectKeyIdentifier = hash
 authorityKeyIdentifier = keyid,issuer
 keyUsage = critical, digitalSignature, keyEncipherment
-
-
 [ crl_ext ]
 # Extension for CRLs (`man x509v3_config`).
 authorityKeyIdentifier=keyid:always
-
 [ ocsp ]
 # Extension for OCSP signing certificates (`man ocsp`).
 basicConstraints = CA:FALSE
@@ -307,8 +268,8 @@ mkdir certs crl csr newcerts private
 chmod 700 private
 touch index.txt
 echo 1000 > serial
-echo 1000 > crlnumber
-cd ../
+echo 1000 > ~/ca/intermediate/crlnumber
+cd ~/ca
 openssl genrsa -aes256 -passout pass:1234\
       -out intermediate/private/intermediate.key.pem 4096
 chmod 400 intermediate/private/intermediate.key.pem
@@ -330,3 +291,5 @@ openssl verify -CAfile certs/ca.cert.pem \
 cat intermediate/certs/intermediate.cert.pem \
       certs/ca.cert.pem > intermediate/certs/ca-chain.cert.pem
 chmod 444 intermediate/certs/ca-chain.cert.pem
+
+mv -r ~/ca $cwd/ca
