295d8c07a7df65ce0043bfb01f6f8c65aef29c12
Ainaijugen <haotang.liu@gmail.com>
2020-12-22 05:13:36 +0000

    update csr timestamp

--- server/create-folders.sh
+++ server/create-folders.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# mkdir messages
-# mkdir tmp
-# mkdir certs
+mkdir messages
+mkdir tmp
+mkdir certs
 
 input=("bob" "addleness" "analects" "annalistic" "anthropomorphologically" "blepharosphincterectomy" "corector" "durwaun" "dysphasia" "encampment" "endoscopic" "exilic" "forfend" "gorbellied" "gushiness" "muermo" "neckar" "outmate" "outroll" "overrich" "philosophicotheological" "pockwood" "polypose" "refluxed" "reinsure" "repine" "scerne" "starshine" "unauthoritativeness" "unminced" "unrosed" "untranquil" "urushinic" "vegetocarbonaceous" "wamara" "whaledom")
 
