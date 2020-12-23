3a1a22514ed39d258945725057d5f5a0e7edb1bb
Ainaijugen <haotang.liu@gmail.com>
2020-12-23 07:48:45 +0000

    add client opensslconfig

--- client/cgencsr.sh
+++ client/cgencsr.sh
@@ -8,7 +8,7 @@ echo $ts
 openssl genrsa -out client_files/key.pem 2048
 chmod 400 client_files/key.pem
 
-openssl req -config ../ca/intermediate/openssl.cnf \
+openssl req -config openssl.cnf \
       -key client_files/key.pem \
       -new -sha256 -out client_files/csr.pem \
       -subj "/C=US/ST=California/L=LA/O=$1/OU=$1.$ts/CN=$1"
