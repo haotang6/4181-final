895bf31537a42ba67c2e5f24b63c1ef6dc49e918
Weifan Jiang <weifanjiang@hotmail.com>
2020-12-22 19:02:10 -0500

    added password

---
+++ CAserver/password_permissions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# add a group that has access to the user passwords
+random="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)"
+groupname="CAserver_$random"
+grep -E -i "^$groupname" /etc/group;
+while [ $? -eq 0 ]
+do
+  random="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)"
+  groupname="CAserver_$random"
+  grep -E -i "^$groupname" /etc/group;
+done
+addgroup --force-badname "$groupname"
+
+chown root user_passwords.txt
+chgrp "$groupname" user_passwords.txt
+chmod u=rwx,g=rwx user_passwords.txt
